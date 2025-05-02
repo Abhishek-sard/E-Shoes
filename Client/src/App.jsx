67bc33f8544181355f18894b4c5945604d9e9d0d
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-05-02 13:21:37 +0545

    push

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import Navbar from "./Component/Navbar/Navbar"
+
 function App() {
 
 
   return (
     <>
-
+      <Navbar/>
     </>
   )
 }
---
+++ Client/src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-[#07004D] text-white'>
+        {/*Top Navigation bar*/}
+        <div className='flex items-center justify-between px-6 py-4'>
+            {/* logo */}
+            <div className='flex items-center space-x-2 text-2*l font-bold'>
+                <span className='text-white'>built</span>
+                <div className='border border-blue-500 h-6 w-6 border-r-0 border-t-0'></div>
+
+                
+
+            </div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
